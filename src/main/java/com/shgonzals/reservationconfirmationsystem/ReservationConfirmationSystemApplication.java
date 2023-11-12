b19fc4a494e600aea64d92c8c7606aa584dc1236
shgonzals <sh.gonzal.s@gmail.com>
2023-11-12 18:39:49 +0100

    initial commit

---
+++ src/main/java/com/shgonzals/reservationconfirmationsystem/ReservationConfirmationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.shgonzals.reservationconfirmationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationConfirmationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationConfirmationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shgonzals/reservationconfirmationsystem/ReservationConfirmationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shgonzals.reservationconfirmationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationConfirmationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
